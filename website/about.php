18a0f842f7dd5fa2fd9903aacf9f6d436f1aa737
Chirag Murari <84890563+Chirag-Murari@users.noreply.github.com>
2022-11-12 19:59:18 +0530

    Add files via upload

---
+++ website/about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Samsung plaza</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<table width="1000" border="0" cellspacing="0" cellpadding="0" align="center" class="tblmain effect8">
+  <?php include('headpart.php'); ?>
+  <tr>
+    <td colspan="3" align="center" height="250"><img src="img/banner.jpg" /></td>
+  </tr>
+  
+  <tr>
+  <td colspan="3">
+  <table width="950" border="0" cellspacing="0" cellpadding="0" align="center">
+  <tr>
+    <td width="700"><h3>About Us</h3>
+	<p style="text-align:justify; padding-right:15px">At Samsug plaza is a place, where we don&rsquo;t just sale the products but we also make people happy by providing nice service. Samsung plaza  was built on the belief that Samsung products should provide an easy and convinent method to buy specific company products, and we carry that belief into everything we do. With more than 5 years of   experience under our belts, we understand how to best serve our customers through tried and true service principles:&nbsp;<strong>We deliver products we&rsquo;re proud to serve and deliver it fast, with a smile.</strong></p>
+	<h3>From the heart of Director</h3>
+ <p style="text-align:justify; padding-right:15px">
+It all started with a spark of idea came in my mind to provide a hassale free product delivery when i was waiting for my product for a long time.I want to create an easy and convinent way of shopping portalfor a company.
+I began my working career as a garment merchant and transport contractor. However, I always had my mind on creating/developinga website portal -creating it for my family and friends,
+I made the decision to start my developing,  With my friend's support, I went to Ahmedabad to pursue development,
+I worked with the developers at Ahmedabad, I developed a range of skills with a great user-friendly website navigation. the most important to element for a good website is having user-friendly navigation. That have become the foundation for for the websites.</p>
+ <p style="text-align:justify; padding-right:15px">Without the help and unending support of my dear friends and family,  my dream would never have come true.  Their encouragement is so important to me. 
+   I would like to express my gratitude to my mother,  And I would like to thank you my friends,
+   My dearest daddy  His love and guidance are always with me.  He once said to me, “I cannot give you much, but I will support you as far as you want. These words echo in my heart everyday.  He is my life role model.  I will never forget his love, his perseverance, and his positive attitude toward life. <br /><br />
+   <b>- Chirag B. Murari</b> </p></td>
+    <td width="250" align="center">
+	<img src="img/abt.png" width="70%" />
+	</td>
+  </tr>
+</table>
+
+	</td>
+  </tr>
+  <tr><td colspan="3">&nbsp;  </td></tr>
+  <?php include('footpart.php'); ?>
+</table>
+
+</body>
+</html>
