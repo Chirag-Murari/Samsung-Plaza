22963d14f07442ccb6cdbac2e218311085f4ffd6
Chirag Murari <84890563+Chirag-Murari@users.noreply.github.com>
2022-12-08 17:41:06 +0530

    Add files via upload

---
+++ website/headpart_1.php
@@ -0,0 +1,66 @@
+<style type="text/css">
+		img.icon {
+		border:none;
+		width:40px;
+		height:40px;
+		padding:25px 5px 8px 5px;
+		}
+		
+		.ext {
+			padding:10px;
+			font-weight:bold;
+			border:#666 solid thin;
+		}
+		.ext:hover {
+			
+			font-weight:bold;
+			background-color:#666;
+			color:#FFFFFF;
+			border:#000 solid thin;
+		}
+		
+	</style>
+	<tr>
+    <td width="40">&nbsp;</td>
+    <td width="160">
+		<a href="index.php"><img src="img/logoo.png" /></a>	</td>
+    <td width="800" align="center" style="vertical-align:top; padding-top:50px;">
+	<div class="user">
+	<?php
+		@ session_start();
+		
+		@$user=$_SESSION['user'];
+		if (@$_SESSION['user'] == '')
+		{
+			$user='Guest';
+			echo "Welcome to $user";  
+		}
+		else
+		{
+		include('connect_str.php');		
+		$qry="select * from register where uname='$user'";
+		$rcc=mysql_query($qry);
+		$rw=mysql_fetch_array($rcc);
+		$nm = $rw[0]." ".$rw[1];
+		echo "Welcome to ".strtoupper($nm)." | <a href='logout.php'>Logout</a>";  
+		}
+	?>
+   </br></br>
+	<div class="navigation">
+	<ul>
+		<li><a href="index.php">Home</a></li>
+		<li><a href="about.php">About Us</a></li>
+		<li><a href="viewcart.php">View Cart</a></li>
+		<li><a href="review.php">Review</a></li>
+		<li><a href="contact.php">Contact</a></li>
+	</ul>
+	<br /><br /><br />
+	
+	</div>	
+	
+			<a href="mobiles.php">Mobiles</a>
+			<a href="tv.php">Tvs </a>
+			<a href="sw.php">Smartwatches</a>
+		
+	</td>
+  </tr>
---
+++ website/index_1.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>samsung plaza</title>
+<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<table width="1000" border="0" cellspacing="0" cellpadding="0" align="center" class="tblmain effect8">
+<?php include('headpart_1.php'); ?>
+  
+  <tr>
+    <td colspan="3" align="center"><img src="img/banner.jpg" /></td>
+  </tr>
+  
+  <tr>
+  <td colspan="3">
+  	<table width="950" border="0" cellspacing="0" cellpadding="0" align="center" style="text-align:center">
+      <tr>
+        <td colspan="4"><h2 >Customer Like Most Items</h2></td>
+        </tr>
+      <tr>
+        <td width="247" height="30">Samsung Galaxy M30s</td>
+        <td width="247" height="30">Samsung Galaxy TV 40" </td>
+        <td width="247" height="30">Samsung Galaxy Note 9</td>
+        <td width="247" height="30">Samsung Galaxy TV 43"</td>
+      </tr>
+      <tr>
+        <td width="247"><img src="products/sGm30s (2).jpg" class="thumb" style="height:250px;width:300px"/></td>
+        <td width="247"><img src="products/sTv_40_i_.jpg" class="thumb" /></td>
+        <td width="247">
+		<img src="products/sGn9_2.jpg" class="thumb" style="height:250px;width:150px"  />		</td>
+        <td width="247">
+		<img src="products/sTv_43_i.jpg" class="thumb" />		</td>
+      </tr>
+      <tr>
+        <td height="30"><span class="price">Price : 13,999</span> </td>
+        <td height="30"><span class="price">Price : 28,999</span> </td>
+		<td height="30"><span class="price">Price : 41,999</span> </td>
+		<td height="30"><span class="price">Price : 33,499 </span> </td>
+      </tr>
+      <tr>
+        <td height="45"><a href='insertcart.php?id=sGm30s'><img src="img/cart.png" /></a></td>
+        <td height="45"><a href='insertcart.php?id=pz11'><img src="img/cart.png" /></a></td>
+        <td height="45"><a href='insertcart.php?id=pz04'><img src="img/cart.png" /></a></td>
+        <td height="45"><a href='insertcart.php?id=pz02'><img src="img/cart.png" /></a></td>
+      </tr>
+	  <tr>
+        <td width="247" height="30" class="brdtop">Samsung smart Tv 40"</td>
+        <td width="247" height="30" class="brdtop">Samsung Galaxy Note 10</td>
+        <td width="247" height="30" class="brdtop">Samsung Galaxy Tv 55"</td>
+        <td width="247" height="30" class="brdtop">Samsung Galaxy Note 10+</td>
+      </tr>
+      <tr>
+        <td width="247">
+		<img src="products/sTv_50_i_.jpg" class="thumb" />		</td>
+        <td width="247">
+		<img  src="products/sGn10.jpg" class="thumb" height="1000px"/>		</td>
+        <td width="247">
+		<img src="products/sTv_50_i_.jpg" class="thumb" />		</td>
+        <td width="247">
+		<img src="products/sGn10.jpg" class="thumb" />		</td>
+      </tr>
+      <tr>
+        <td height="30"><span class="price">Price : 30,000 </span> </td>
+        <td height="30"><span class="price">Price : 69,999 </span> </td>
+		<td height="30"><span class="price">Price : 65,800 </span> </td>
+		<td height="30"><span class="price">Price : 79,999 </span> </td>
+      </tr>
+      <tr>
+        <td height="45"><a href='insertcart.php?id=st02'><img src="img/cart.png" /></a></td>
+        <td height="45"><a href='insertcart.php?id=st03'><img src="img/cart.png" /></a></td>
+        <td height="45"><a href='insertcart.php?id=st08'><img src="img/cart.png" /></a></td>
+        <td height="45"><a href='insertcart.php?id=bv03'><img src="img/cart.png" /></a></td>
+      </tr>
+    </table>  </td>
+  </tr>
+  <?php include('footpart.php'); ?> 
+</table>
+
+</body>
+</html>
